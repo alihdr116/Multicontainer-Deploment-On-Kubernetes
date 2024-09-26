2e0e548bb31163ac189b129afbe6cf0a1ac8932c
Ali Haider <iam.alihaider921@gmail.com>
2024-09-27 03:56:53 +0500

    Running_Web

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const conn = mysql.createConnection({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
+    user:"ali",
+    host:"localhost",
+    password:"root",
+    database:"crudmysql"
 });
 
 
