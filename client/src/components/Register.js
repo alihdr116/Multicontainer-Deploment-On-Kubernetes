c939c5b7e92239f0b6f362e30ca34f701103524c
Ali Haider <iam.alihaider921@gmail.com>
2024-10-01 05:47:38 +0500

    Ready-to-Deploy

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,9 @@
-import React, { useContext, useState } from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
+import React, { useContext, useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import { adddata } from './context/ContextProvider';
 
 const Register = () => {
-
     const { udata, setUdata } = useContext(adddata);
-
     const history = useHistory();
 
     const [inpval, setINP] = useState({
@@ -16,43 +14,38 @@ const Register = () => {
         work: "",
         add: "",
         desc: ""
-    })
+    });
 
     const setdata = (e) => {
-        console.log(e.target.value);
         const { name, value } = e.target;
-        setINP((preval) => {
-            return {
-                ...preval,
-                [name]: value
-            }
-        })
-    }
-
+        setINP((preval) => ({
+            ...preval,
+            [name]: value
+        }));
+    };
 
     const addinpdata = async (e) => {
         e.preventDefault();
 
         const { name, email, work, add, mobile, desc, age } = inpval;
 
-
-        if (name == "") {
-            alert("name is required")
-        } else if (email == "") {
-            alert("email is required")
+        if (name === "") {
+            alert("Name is required");
+        } else if (email === "") {
+            alert("Email is required");
         } else if (!email.includes("@")) {
-            alert("enter valid email")
-        } else if (work == "") {
-            alert("work is required")
-        } else if (add == "") {
-            alert("add is required")
-        } else if (mobile == "") {
-            alert("mobile is required")
-        } else if (age == "") {
-            alert("age is required")
+            alert("Enter a valid email");
+        } else if (work === "") {
+            alert("Work is required");
+        } else if (add === "") {
+            alert("Address is required");
+        } else if (mobile === "") {
+            alert("Mobile is required");
+        } else if (age === "") {
+            alert("Age is required");
         } else {
-
             const res = await fetch("/create", {
+
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -66,57 +59,54 @@ const Register = () => {
             console.log(data);
 
             if (res.status === 422 || !data) {
-                console.log("error ");
-                alert("error");
-
+                console.log("Error");
+                alert("Error");
             } else {
-                history.push("/")
-                setUdata(data)
-                console.log("data added");
-
+                history.push("/");
+                setUdata(data);
+                console.log("Data added");
             }
         }
-
-    }
+    };
 
     return (
         <div className="container">
-            <NavLink to="/">home</NavLink>
+            <NavLink to="/">Home</NavLink>
             <form className="mt-4">
                 <div className="row">
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputEmail1" class="form-label">Name</label>
-                        <input type="text" value={inpval.name} onChange={setdata} name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div className="mb-3 col-lg-6 col-md-6 col-12">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" value={inpval.name} onChange={setdata} name="name" className="form-control" id="name" />
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">email</label>
-                        <input type="email" value={inpval.email} onChange={setdata} name="email" class="form-control" id="exampleInputPassword1" />
+                    <div className="mb-3 col-lg-6 col-md-6 col-12">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" value={inpval.email} onChange={setdata} name="email" className="form-control" id="email" />
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">age</label>
-                        <input type="text" value={inpval.age} onChange={setdata} name="age" class="form-control" id="exampleInputPassword1" />
+                    <div className="mb-3 col-lg-6 col-md-6 col-12">
+                        <label htmlFor="age" className="form-label">Age</label>
+                        <input type="text" value={inpval.age} onChange={setdata} name="age" className="form-control" id="age" />
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Mobile</label>
-                        <input type="number" value={inpval.mobile} onChange={setdata} name="mobile" class="form-control" id="exampleInputPassword1" />
+                    <div className="mb-3 col-lg-6 col-md-6 col-12">
+                        <label htmlFor="mobile" className="form-label">Mobile</label>
+                        <input type="number" value={inpval.mobile} onChange={setdata} name="mobile" className="form-control" id="mobile" />
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Work</label>
-                        <input type="text" value={inpval.work} onChange={setdata} name="work" class="form-control" id="exampleInputPassword1" />
+                    <div className="mb-3 col-lg-6 col-md-6 col-12">
+                        <label htmlFor="work" className="form-label">Work</label>
+                        <input type="text" value={inpval.work} onChange={setdata} name="work" className="form-control" id="work" />
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Address</label>
-                        <input type="text" value={inpval.add} onChange={setdata} name="add" class="form-control" id="exampleInputPassword1" />
+                    <div className="mb-3 col-lg-6 col-md-6 col-12">
+                        <label htmlFor="address" className="form-label">Address</label>
+                        <input type="text" value={inpval.add} onChange={setdata} name="add" className="form-control" id="address" />
                     </div>
-                    <div class="mb-3 col-lg-12 col-md-12 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Description</label>
-                        <textarea name="desc" value={inpval.desc} onChange={setdata} className="form-control" id="" cols="30" rows="5"></textarea>
+                    <div className="mb-3 col-lg-12 col-md-12 col-12">
+                        <label htmlFor="desc" className="form-label">Description</label>
+                        <textarea name="desc" value={inpval.desc} onChange={setdata} className="form-control" id="desc" cols="30" rows="5"></textarea>
                     </div>
-
-                    <button type="submit" onClick={addinpdata} class="btn btn-primary">Submit</button>
+                    <button type="submit" onClick={addinpdata} className="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
-    )
+    );
 }
+
 export default Register;
